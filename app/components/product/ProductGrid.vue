2995074b965b12c0f34ea80f172cdd223ba3a8be
mors <seyedsahel1383@gmail.com>
2026-07-27 11:53:23 +0330

    fix-UI(product grid): center the title

--- app/components/product/ProductGrid.vue
+++ app/components/product/ProductGrid.vue
@@ -11,9 +11,11 @@ const products = computed(() => productStore.byCategory[props.category] ?? [])
 
 <template>
   <section class="max-w-full mx-auto px-6 sm:px-8 lg:px-16 py-10">
-    <h2 v-if="title" class="text-lg sm:text-2xl font-semibold text-text-primary mb-5">
-      {{ title }}
-    </h2>
+    <div class="flex justify-center items-center mb-7">
+      <h2 v-if="title" class="text-lg sm:text-2xl font-semibold text-text-primary">
+        {{ title }}
+      </h2>
+    </div>
 
     <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 gap-4">
       <template v-if="productStore.isLoading">
