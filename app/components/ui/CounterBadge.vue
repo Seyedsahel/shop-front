66f0742e95b9a447b1d772081c6895c351ed732a
mors <seyedsahel1383@gmail.com>
2026-08-25 09:17:24 +0330

    fix-UI(counterBadge): resize in desktop and mobile view

--- app/components/ui/CounterBadge.vue
+++ app/components/ui/CounterBadge.vue
@@ -5,7 +5,7 @@ defineProps<{ count: number }>()
 <template>
   <span
     v-if="count > 0"
-    class="absolute -top-1.5 -inset-e-1.5 min-w-4 h-4 px-1 rounded-full bg-danger-subtle text-text-on-dark text-[10px] font-extrabold flex items-center justify-center"
+    class="absolute -top-1.5 -inset-e-1.5 min-w-4 h-4 lg:min-w-5 lg:h-5 px-1 rounded-full bg-danger-subtle  text-text-on-dark text-[10px] font-extrabold flex items-center justify-center"
   >
     {{ count > 99 ? '99+' : count }}
   </span>
