a3747585ff49b099c881efe046592919ff8266c8
mors <seyedsahel1383@gmail.com>
2026-08-31 17:44:33 +0330

    fix-UI(home page):delete product grid, not needed here anymore

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,7 +7,7 @@
     <BannerDuoBanner />
     <ProductSlider category="home-product" title="محصولات ویژه" variant="compact"  />
     <BrandSlider />
-    <ProductGrid category="home-product" title="محصولات ویژه" variant="compact" />
+    <!-- <ProductGrid category="home-product" title="محصولات ویژه" variant="compact" /> -->
     <HomeConsultationSection/>
     <BlogGrid/>
     <HomeBenefitsSection/>
